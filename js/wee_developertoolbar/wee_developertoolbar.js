fa42819ecab816ea87f6bf3105b099fcc90b37aa
Vincent Pietri <vincent.pietri@gmail.com>
2015-03-24 22:13:57 +0100

    Cleaning and finalize log display

--- js/wee_developertoolbar/wee_developertoolbar.js
+++ js/wee_developertoolbar/wee_developertoolbar.js
@@ -39,11 +39,13 @@ DevToolbar.prototype = {
             });
         });
         
+        /*
         $('tabContent_blocks').select('a.toggleBlogProperties').each( function(element){
             element.observe("click",function(event){
                 Event.element(event).adjacent("ul.eventProperties").first().toggle(); 
             });
         });
+        */
         
         $('tabContent_events').select('a.toggleBlogProperties').each( function(element){
             element.observe("click",function(event){
@@ -132,67 +134,3 @@ document.observe("dom:loaded", function() {
         var toolbar = new DevToolbar();
     }
 });
-
-
-/*
-
-jQuery(document).ready(function(){
-
-  if (Cookie.read("wee_developertoolbar") == 0)    {
-      jQuery("#weeDeveloperToolbar").hide();  
-      jQuery("#weeDeveloperToolbarPoweredBy").hide();  
-  }
-
-  jQuery("#weeDeveloperToolbarContainer img:first").click(function() {
-    jQuery(".weeDeveloperToolbarDetails").hide();
-    jQuery("#weeDeveloperToolbar").toggle();
-    jQuery("#weeDeveloperToolbarPoweredBy").toggle();
-    var display = jQuery("#weeDeveloperToolbar").attr("style");
-    var toolbarHiddenExpression = /(none)/;
-    if (toolbarHiddenExpression.exec(display)) {
-      Cookie.write("wee_developertoolbar", 0);
-    } else {
-      Cookie.write("wee_developertoolbar", 1);    
-    }
-  });    
-  
-  jQuery("ul.tabContainer li").click(function() {
-    var id = jQuery(this).attr("id").split("_");
-    id = id[1];
-    var parent = jQuery(this).parent().parent();
-    parentContainerId = jQuery(parent).attr("id");
-    jQuery("#"+parentContainerId+ " ul.tabContainer li").removeClass("active");
-    jQuery(this).addClass("active"); 
-    var index = jQuery("#"+parentContainerId+ " ul.tabContainer li").index(this);
-    jQuery("#"+parentContainerId+ " .tabContent").hide();
-    jQuery("#tabContent_"+id).show();
-  });
-    
-  jQuery("#weeDeveloperToolbar li.content").click(function() {
-    var id = jQuery(this).attr("id").split("_");
-    id = id[1];
-    jQuery(".weeDeveloperToolbarDetails").each(function(e) {
-      var toolbarDetailContainer = jQuery(".weeDeveloperToolbarDetails").get(e);
-      if (jQuery(toolbarDetailContainer).attr("id") != "weeDeveloperToolbarDetails_"+id) {
-        jQuery(toolbarDetailContainer).hide();     
-      }
-    });
-    if (jQuery("#weeDeveloperToolbarDetails_"+id)) {
-      jQuery("#weeDeveloperToolbarDetails_"+id).toggle();    
-    }
-  });
-  
-  jQuery("#tabContent_blocks a.toggleBlogProperties").click(function() {
-    jQuery(this).next("ul.blockProperties").toggle(); 
-  });
-  
-  jQuery("#tabContent_blocks a.toggleBlogProperties").click(function() {
-	    jQuery(this).next("ul.eventProperties").toggle(); 
-  });
-  
-  jQuery("#tabContent_events a.toggleBlogProperties").click(function() {
-	    jQuery(this).next("ul.events").toggle(); 
-});
-  
-});
-*/
